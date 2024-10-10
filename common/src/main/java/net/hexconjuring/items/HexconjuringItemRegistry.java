e32131551620a04d8d600ee8b42e68d56c0d855f
demonpie0369 <finlayirvine0369@gmail.com>
2024-10-11 12:52:18 +1300

    yeah

--- common/src/main/java/net/hexconjuring/items/HexconjuringItemRegistry.java
+++ common/src/main/java/net/hexconjuring/items/HexconjuringItemRegistry.java
@@ -14,11 +14,11 @@ import at.petrak.hexcasting.common.items.magic.ItemMediaHolder;
 public class HexconjuringItemRegistry {
     // Register items through this
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Hexconjuring.MOD_ID, Registry.ITEM_KEY);
-    private static ConjuredPickaxeItem conjuredpickaxe = new ConjuredPickaxeItem(ModToolMaterials.CONJUREDTOOL, 0, 0, new Item.Settings().maxCount(1));
-    private static ConjuredAxeItem conjuredaxe = new ConjuredAxeItem(ModToolMaterials.CONJUREDTOOL, 4, 0, new Item.Settings().maxCount(1));
-    private static ConjuredHoeItem conjuredhoe = new ConjuredHoeItem(ModToolMaterials.CONJUREDTOOL, 0, 0, new Item.Settings().maxCount(1));
-    private static ConjuredShovelItem conjuredshovel = new ConjuredShovelItem(ModToolMaterials.CONJUREDTOOL, 0, 0, new Item.Settings().maxCount(1));
-    private static ConjuredSwordItem conjuredsword = new ConjuredSwordItem(ModToolMaterials.CONJUREDTOOL, 6, -2.4f, new Item.Settings().maxCount(1));
+    //private static final ConjuredPickaxeItem conjuredpickaxe = new ConjuredPickaxeItem(ModToolMaterials.CONJUREDTOOL, 0, 0, new Item.Settings().maxCount(1));
+    //private static final ConjuredAxeItem conjuredaxe = new ConjuredAxeItem(ModToolMaterials.CONJUREDTOOL, 4, 0, new Item.Settings().maxCount(1));
+    //private static final ConjuredHoeItem conjuredhoe = new ConjuredHoeItem(ModToolMaterials.CONJUREDTOOL, 0, 0, new Item.Settings().maxCount(1));
+    //private static final ConjuredShovelItem conjuredshovel = new ConjuredShovelItem(ModToolMaterials.CONJUREDTOOL, 0, 0, new Item.Settings().maxCount(1));
+    //private static final ConjuredSwordItem conjuredsword = new ConjuredSwordItem(ModToolMaterials.CONJUREDTOOL, 6, -2.4f, new Item.Settings().maxCount(1));
 
     public static void init() {
         ITEMS.register();
@@ -29,15 +29,15 @@ public class HexconjuringItemRegistry {
 
     // During the loading phase, refrain from accessing sueppliers' items (e.g. EXAMPLE_ITEM.gt()), they will not be available
     public static final RegistrySupplier<ConjuredPickaxeItem> CONJURED_PICKAXE = ITEMS.register("conjured_pickaxe",
-        () -> conjuredpickaxe);
+        () -> new ConjuredPickaxeItem(ModToolMaterials.CONJUREDTOOL, 0, 0, new Item.Settings().maxCount(1)));
     public static final RegistrySupplier<ConjuredAxeItem> CONJURED_AXE = ITEMS.register("conjured_axe",
-        () -> conjuredaxe);
+        () -> new ConjuredAxeItem(ModToolMaterials.CONJUREDTOOL, 4, 0, new Item.Settings().maxCount(1)));
     public static final RegistrySupplier<ConjuredHoeItem> CONJURED_HOE = ITEMS.register("conjured_hoe",
-            () -> conjuredhoe);
+            () -> new ConjuredHoeItem(ModToolMaterials.CONJUREDTOOL, 0, 0, new Item.Settings().maxCount(1)));
     public static final RegistrySupplier<ConjuredShovelItem> CONJURED_SHOVEL = ITEMS.register("conjured_shovel",
-            () -> conjuredshovel);
+            () -> new ConjuredShovelItem(ModToolMaterials.CONJUREDTOOL, 0, 0, new Item.Settings().maxCount(1)));
     public static final RegistrySupplier<ConjuredSwordItem> CONJURED_SWORD = ITEMS.register("conjured_sword",
-            () -> conjuredsword);
+            () -> new ConjuredSwordItem(ModToolMaterials.CONJUREDTOOL, 6, -2.4f, new Item.Settings().maxCount(1)));
 
 
 }
